64c3d5d16dc4097905cbefebcb3e85e47ad41541
Kyle Spencer <spencerk2@vcu.edu>
2019-01-21 23:51:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ def countdown(integer)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(integer)
+  count = integer
+  while count >= 0
+  puts "#{count} SECOND(S)!"
+  count -= 1
+  sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
